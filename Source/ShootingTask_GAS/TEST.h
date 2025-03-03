7b171e9874045a62010f9aeaba281cc00c3e028e
sdfrwifu <nikonov177@yandex.ru>
2025-03-03 10:27:33 +0400

    -
    -

---
+++ Source/ShootingTask_GAS/TEST.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "TEST.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGTASK_GAS_API UTEST : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ShootingTask_GAS/testgas.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "testgas.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGTASK_GAS_API Utestgas : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
